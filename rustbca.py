588e708fb8f0af3579951382c9ea852f0455c63f
Davide Curreli <dcurreli@illinois.edu>
2022-10-04 16:20:54 -0500

    Update rustbca.py

--- rustbca.py
+++ rustbca.py
@@ -1606,9 +1606,9 @@ def test():
 def main():
     ions = [carbon]   # list of ions, e.g.: ions=[hydrogen, deuterium, lithium]
     target = tungsten
-    energies = np.logspace(0, 3, 5)
-    angles = np.array([0.00001, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 85.0])
-    number_ions = 1000
+    energies = np.logspace(1, 3, 100)
+    angles = np.array([0.00001, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 65.0, 70.0, 72.0, 74.0, 76.0, 78.0, 80.0, 82.0, 84.0, 86.0])
+    number_ions = 10000
     num_energies = energies.size
     num_angles   = angles.size
     legends = []
